f92cd41087034a56cba23d8c37917732c1b788b1
joshbacon <jdbacon@mta.ca>
2022-10-24 13:47:17 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,91 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>code_gambit</h1>
       </header>
+
+      <div className="board">
+        <div className="row">
+          <div className="square">0</div>
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+        </div>
+        <div className="row">
+          <div className="square">0</div>
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+        </div>
+        <div className="row">
+          <div className="square">0</div>
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+        </div>
+        <div className="row">
+          <div className="square">0</div>
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+        </div>
+        <div className="row">
+          <div className="square">0</div>
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+        </div>
+        <div className="row">
+          <div className="square">0</div>
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+        </div>
+        <div className="row">
+          <div className="square">0</div>
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+        </div>
+        <div className="row">
+          <div className="square">0</div>
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+        </div>
+      </div>
     </div>
   );
 }
