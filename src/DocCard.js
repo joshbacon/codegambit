1ccf19069cb8d49fb828ad50ad09b11777559029
joshbacon <jdbacon@mta.ca>
2022-10-26 23:38:34 -0300

    started on doc cards

--- src/DocCard.js
+++ src/DocCard.js
@@ -1,5 +1,12 @@
 import './DocCard.css';
 
-const Docs = (props) => {
+const DocCard = (props) => {
+    let method = props.method;
+    console.log(method);
     
+    return <div className='card'>
+        {method.method}
+    </div>
 }
+
+export default DocCard;
--- src/Docs.js
+++ src/Docs.js
@@ -1,5 +1,5 @@
 import './Docs.css';
-import { DocCard } from './DocCard.js'
+import DocCard from './DocCard.js'
 import DocData from './docs.json';
 import React from 'react';
 
@@ -10,60 +10,27 @@ const Docs = (props) => {
             <div className='menu-bar'>
                 <h1>Documentation</h1>
                 {DocData.map((value, key) => {
-                    return <div className='list-section'>
+                    return <div key={key} className='list-section'>
                         <h2>{value.section}</h2>
                         <ul>
                             {value.methods.map((value, key) => {
-                                return <li className='method'> - {value.method}</li>
+                                return <li key={key} className='method'> - {value.method}</li>
                             })}
                         </ul>
                     </div>
                 })}
-                {/* <div className='list-section'>
-                    <h2>Selection</h2>
-                    <ul>
-                        <li className='method'> - select()</li>
-                        <li className='method'> - selected()</li>
-                        <li className='method'> - unselect()</li>
-                    </ul>
-                </div>
-                <div className='list-section'>
-                    <h2>Movement</h2>
-                    <ul>
-                        <li className='method'> - move()</li>
-                        <li className='method'> - take()</li>
-                    </ul>
-                </div>
-                <div className='list-section'>
-                    <h2>Checking</h2>
-                    <ul>
-                        <li className='method'> - isValidMove()</li>
-                        <li className='method'> - showValidMoves()</li>
-                    </ul>
-                </div>
-                <div className='list-section'>
-                    <h2>Gameplay</h2>
-                    <ul>
-                        <li className='method'> - offerDraw()</li>
-                        <li className='method'> - resign()</li>
-                    </ul>
-                </div> */}
             </div>
 
-            <div>
-                <section className='selection'>
-                    <h1>Selection</h1>
-                    <p></p>
-                </section>
-                <section className='movement'>
-                    <h1>Movement</h1>
-                </section>
-                <section className='checking'>
-                    <h1>Checking</h1>
-                </section>
-                <section className='gameplay'>
-                    <h1>Gameplay</h1>
-                </section>
+            <div className='main'>
+                {DocData.map((value, key) => {
+                    // make a section and map on each of the methods for a card.
+                    return <section key={key}>
+                        <h2>{value.section}</h2>
+                        {value.methods.map((value, key) => {
+                            return <DocCard key={key} method={value}/>
+                        })}
+                    </section>
+                })}
             </div>
         </div>
     );
