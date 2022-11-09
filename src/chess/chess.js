1c3266a117a24d7b29cc0da16093066ffd819ebf
joshbacon <jdbacon@mta.ca>
2022-11-09 01:31:05 -0400

    implemented FEN string generation

--- src/chess/chess.js
+++ src/chess/chess.js
@@ -58,7 +58,7 @@ const PIECES = {
     EMPTY:    ''
 }
 
-const INITIAL_FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0'
+const INITIAL_FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
 // FEN Explanation:
 // first field  : pieces (left to right, top to bottom, from whites perspective)
 // second field : whos turn it is
@@ -68,67 +68,7 @@ const INITIAL_FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 0'
 // sixth field  : 'fullmove number', number of moves played this game (increments each time black makes a move)
 
 
-function validateFEN(FEN) {
-    let tokens = FEN.split(' ');
-    
-    if (tokens.length !== 6)
-        return {valid: false, code: 1, error: "FEN string must contain 6 fields."};
 
-    let pieces = tokens[0].split('/');
-    let turn = tokens[1];
-    let castling = tokens[2];
-    let enpassant = tokens[3];
-    let halfmove = tokens[4];
-    let fullmove = tokens[5];
-
-    // Field #1 checking
-    if (pieces.length !== 8)
-        return {valid: false, code: 2, error: "Pieces field must consist of 8 rows."};
-
-    for (r in pieces) {
-        let rowSum = 0;
-        let lastWasNumber = false;
-        for (p in pieces[r]) {
-            let piece = pieces[r][p];
-            if (isNaN(piece)){
-                if (/^[KQRBNPkqrbnp]$/.test(piece)) {
-                    rowSum += 1;
-                    lastWasNumber = false;
-                }
-                else return {valid: false, code: 3, error: "Invalid piece found in pieces field."};
-            } else {
-                if (lastWasNumber)
-                    return {valid: false, code: 4, error: "Consecutive numbers found in pieces field."};
-                rowSum += parseInt(piece);
-                lastWasNumber = true;
-            }
-        }
-        if (rowSum !== 8)
-            return {valid: false, code: 5, error: "Each row section must account for 8 squares."};
-    }
-
-    // Field #2 checking
-    if (turn !== WHITE && turn !== BLACK)
-        return {valid: false, code: 6, error: "Turn field must be either w or b for white or black."};
-
-    // Field #3 checking
-    if (!/^(KQ?k?q?|Qk?q?|kq?|q)|-$/.test(castling))
-        return {valid: false, code: 7, error: "Castle field is invalid."};
-
-    // Field #4 checking
-    if (!/^([a-h][1-8])|-$/.test(enpassant))
-        return {valid: false, code: 8, error: "enpassant field is invalid."};
-    
-    // Field #5 checking
-    if (isNaN(halfmove) || parseInt(halfmove) < 0 || parseInt(halfmove) > 99)
-        return {valid: false, code: 9, error: "Halfmove field must be an integer in the range [0, 100)"};
-
-    // Field #6 checking
-    if (isNaN(fullmove) || parseInt(fullmove) < 0)
-        return {valid: false, code: 10, error: "Fullmove field must be an integer greater than 0"};
-
-    return {valid: true, code: 0, error:"No error."};
-}
 
 
 
@@ -153,8 +93,10 @@ const Chess = function (FEN) {
         gameFromFEN(INITIAL_FEN);
     }
     console.log(board);
+    console.log(INITIAL_FEN);
+    console.log(generateFEN());
 
-    function gameFromFEN(FEN){
+    function gameFromFEN(FEN) {
         
         let tokens = FEN.split(' ');
         
@@ -177,9 +119,108 @@ const Chess = function (FEN) {
 
     }
 
-    function isValidMove(move) {
-        // takes in a single move string and returns true or false depending on if it's valid or not
+    function generateFEN() {
+        let FEN = '';
+        // add slash separated pieces
+        let rowCounter = 0;
+        let spaceCounter = 0;
+        for (i in board) {
+            if (rowCounter === 8) {
+                if (spaceCounter !== 0) {
+                    FEN += spaceCounter;
+                    spaceCounter = 0;
+                }
+                FEN += '/';
+                rowCounter = 0;
+            }
+
+            if (board[i] === '') {
+                spaceCounter += 1;
+            } else {
+                if (spaceCounter !== 0) {
+                    FEN += spaceCounter;
+                    spaceCounter = 0;
+                }
+                FEN += board[i];
+            }
+
+            rowCounter += 1;
+        }
+        // add turn
+        FEN += " "+turn;
+        // add castling
+        FEN += " "+castling;
+        // add enpassant
+        FEN += " "+enpassant;
+        // add halfmove
+        FEN += " "+halfmove;
+        // add fullmove
+        FEN += " "+fullmove;
+        return FEN;
     }
+
+    function validateFEN(FEN) {
+        let tokens = FEN.split(' ');
+        
+        if (tokens.length !== 6)
+            return {valid: false, code: 1, error: "FEN string must contain 6 fields."};
+    
+        let pieces = tokens[0].split('/');
+        let turn = tokens[1];
+        let castling = tokens[2];
+        let enpassant = tokens[3];
+        let halfmove = tokens[4];
+        let fullmove = tokens[5];
+    
+        // Field #1 checking
+        if (pieces.length !== 8)
+            return {valid: false, code: 2, error: "Pieces field must consist of 8 rows."};
+    
+        for (r in pieces) {
+            let rowSum = 0;
+            let lastWasNumber = false;
+            for (p in pieces[r]) {
+                let piece = pieces[r][p];
+                if (isNaN(piece)){
+                    if (/^[KQRBNPkqrbnp]$/.test(piece)) {
+                        rowSum += 1;
+                        lastWasNumber = false;
+                    }
+                    else return {valid: false, code: 3, error: "Invalid piece found in pieces field."};
+                } else {
+                    if (lastWasNumber)
+                        return {valid: false, code: 4, error: "Consecutive numbers found in pieces field."};
+                    rowSum += parseInt(piece);
+                    lastWasNumber = true;
+                }
+            }
+            if (rowSum !== 8)
+                return {valid: false, code: 5, error: "Each row section must account for 8 squares."};
+        }
+    
+        // Field #2 checking
+        if (turn !== WHITE && turn !== BLACK)
+            return {valid: false, code: 6, error: "Turn field must be either w or b for white or black."};
+    
+        // Field #3 checking
+        if (!/^(KQ?k?q?|Qk?q?|kq?|q)|-$/.test(castling))
+            return {valid: false, code: 7, error: "Castle field is invalid."};
+    
+        // Field #4 checking
+        if (!/^([a-h][1-8])|-$/.test(enpassant))
+            return {valid: false, code: 8, error: "enpassant field is invalid."};
+        
+        // Field #5 checking
+        if (isNaN(halfmove) || parseInt(halfmove) < 0 || parseInt(halfmove) > 99)
+            return {valid: false, code: 9, error: "Halfmove field must be an integer in the range [0, 100)"};
+    
+        // Field #6 checking
+        if (isNaN(fullmove) || parseInt(fullmove) <= 0)
+            return {valid: false, code: 10, error: "Fullmove field must be an integer greater than 0"};
+    
+        return {valid: true, code: 0, error:"No error."};
+    }
+
 }
 
-Chess('');
+Chess(INITIAL_FEN);
