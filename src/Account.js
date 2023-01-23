484e7667db5bb4e344b11693a5aa50a32a89a2af
joshbacon <jdbacon@mta.ca>
2023-01-22 23:08:54 -0400

    started user card on account page

--- src/Account.js
+++ src/Account.js
@@ -10,18 +10,32 @@ const Account = (props) => {
     // return the account page if one exists
     // otherwise show a login signup page
 
-    // const [settings, toggleSettings] = useState(false);
-
     return <div className='account'>
         <div className='waves'/>
-        {/* { settings && <Settings/> } */}
         <Link to='/' className='home button'>
             <div className='homeImg'/>
         </Link>
-        {/* <button onClick={() => toggleSettings(!settings)} className='settings button'>
-            <div className='settingsImg' />
-        </button> */}
-        <h1>THIS IS YOUR ACCOUNT</h1>
+        <div className='userCard'>
+            <h2>Josh Bacon</h2>
+            <div className='gameSection'>
+                <div className='played section'>
+                    <p className='result'>23</p>
+                    <p>games played</p>
+                </div>
+                <div className='won section'>
+                    <p className='result'>23</p>
+                    <p>games won</p>
+                </div>
+                <div className='winRate section'>
+                    <p className='result'>52%</p>
+                    <p>win rate</p>
+                </div>
+            </div>
+            <div className='lessonSection'>
+                <p className='result'>12</p>
+                <p>Lessons completed</p>
+            </div>
+        </div>
     </div>
 }
 
